a5e2a63b7c68a870870c6341db899b04c06c44dc
Magnum Rocha <magnum.barbosa.rocha@gmail.com>
2022-10-13 08:59:17 +0100

    update to kotlin 1.7.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-    implementation("com.android.tools.build:gradle:7.2.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    implementation("com.android.tools.build:gradle:7.3.0")
     implementation("com.chromaticnoise.multiplatform-swiftpackage:com.chromaticnoise.multiplatform-swiftpackage.gradle.plugin:2.0.3")
 }
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,10 @@
-const val kotlinVersion = "1.7.10"
-const val coroutinesVersion = "1.6.3"
-const val coroutinesMtVersion="1.6.3-native-mt"
+const val kotlinVersion = "1.7.20"
+const val coroutinesVersion = "1.6.4"
+const val coroutinesMtVersion="1.6.4-native-mt"
 
 const val composeVersion = "1.2.1"
-const val composeCompilerVersion = "1.3.0"
+const val composeCompilerVersion = "1.3.2"
 
 const val detektVersion = "1.20.0"
 
-const val kspVersion = "1.7.10-1.0.6"
+const val kspVersion = "1.7.20-1.0.6"
